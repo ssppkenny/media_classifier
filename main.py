1e719a15e237f97a0a68cd9a2c7223d3b2e6859b
Sergey Mikhno (Galexis) <sergey.mikhno@galexis.com>
2021-04-13 07:57:05 +0200

    classify dir

--- main.py
+++ main.py
@@ -3,28 +3,36 @@ import nltk
 import os
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from joblib import dump, load
+from collections import Counter
 
-def prepare_data(filenames, Y, bag, dir_name):
-    tokenizer = nltk.RegexpTokenizer(r"\w+")
-    counter = 1
-    for k, v in bag.items():
-        bag[k] = counter
-        counter+=1
-    X = np.zeros((len(filenames), len(bag)))
-    for i in range(len(filenames)):
-        words = tokenizer.tokenize(filenames[i])
-        X[i,:] = np.array([int(b in words) for b in bag])
-    
-    cls = DecisionTreeClassifier()
-    cls.fit(X,Y)
-    return cls
+extensions = [".pdf", ".epub", ".mp3", ".flac", ".avi", ".mp4", ".mkv"]
+media_type = {0 : "books", 1 : "music", 2 : "Films", 3 : "Audiobooks", 4 : "Series"}
+
+def prepare_data(filenames, Y, bag, from_files=True):
+    if from_files:
+        tokenizer = nltk.RegexpTokenizer(r"\w+")
+        counter = 1
+        for k, v in bag.items():
+            bag[k] = counter
+            counter+=1
+        X = np.zeros((len(filenames), len(bag)))
+        for i in range(len(filenames)):
+            words = tokenizer.tokenize(filenames[i])
+            X[i,:] = np.array([int(b in words) for b in bag])
+        
+        cls = DecisionTreeClassifier()
+        cls.fit(X,Y)
+        dump(cls, '~/.cls.joblib') 
+        return cls
+    else:
+        load('~/.cls.joblib')
 
 def read_dir(dir_name):
     filenames = []
     Y = []
     tokenizer = nltk.RegexpTokenizer(r"\w+")
     bag_of_words = set([])
-    extensions = [".pdf", ".epub", ".mp3", ".flac", ".avi", ".mp4", ".mkv"]
     for root, subdirs, files in os.walk(dir_name):
         for f in files:
             if any([f.endswith(x) for x in extensions]):
@@ -53,6 +61,16 @@ def read_dir(dir_name):
 
     return filenames, Y, dict.fromkeys(bag_of_words,1)
 
+
+def predict_dir(dir_name, cls, bag):
+    for root, subdirs, files in os.walk(dir_name):
+        filenames = []
+        for f in files:
+            if any([f.endswith(x) for x in extensions]):
+                filenames.append(f)
+
+    return {dir_name : media_type[Counter(predict(filenames, cls, bag)).most_common()[0][0]]}
+
 def predict(filenames, cls, bag):
     tokenizer = nltk.RegexpTokenizer(r"\w+")
     X = np.zeros((len(filenames), len(bag)))
@@ -60,22 +78,26 @@ def predict(filenames, cls, bag):
         words = tokenizer.tokenize(filenames[i])
         X[i,:] = np.array([int(b in words) for b in bag])
 
-    return cls.predict(X)
+    d = dict(zip(filenames, cls.predict(X)))
+    return dict((k,media_type[v]) for (k,v) in d.items())
+
+def classify(dir_name, cls, bag):
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    dirs = [f for f in os.listdir('.') if not os.path.isfile(f)]
+
+    d1 = predict(files, cls, bag)
+    d2 = dict([(d,predict_dir(d, cls, bag)[d]) for d in dirs])
+    return {**d1, **d2}
+        
 
 if __name__ == '__main__':
     dir_name = "/home/sergey/Downloads"
     filenames, Y,  bag = read_dir(dir_name)
-    cls = prepare_data(filenames, Y, bag, dir_name)
+    cls = prepare_data(filenames, Y, bag)
 
-    file_name1 = "Java Programming Data.epub".lower()
-    file_name2 = "02. Amadeus Liszt - Win The Race (12'' Version).mp3".lower()
-    file_name3 = "Coding Tricks and Tips March 2020.pdf".lower()
-    file_name4 = "01. Alan Brando - The Same Old Story (Extended Vocal Disco Mix).mp3"
-    file_name5 = "Happily.2021.1080p.WEBRip.x264-RARBG.mp4"
-    file_name6 = "Thundercloud.2015.1080p.AMZN.WEBRip.DDP5.1.x264-ISA.mkv"
-    prediction = predict([file_name1, file_name2, file_name3,
-        file_name4, file_name5, file_name6], cls, bag)
+    prediction = classify(dir_name, cls, bag)
     print(prediction)
+
 
 
 
