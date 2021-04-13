891a389900933e652526c4fa9ea979b3bd4e5d33
Sergey Mikhno (Galexis) <sergey.mikhno@galexis.com>
2021-04-13 14:29:38 +0200

    moving files

--- gui.py
+++ gui.py
@@ -4,6 +4,12 @@ import getpass
 import os
 import classify
 
+def move_files():
+    for k,v in elements:
+        subfolder = k.cget("text")
+        file = v.get()
+        print(f"moving {file} to {subfolder}")
+
 
 window = tk.Tk()
 window.title('Classify')
@@ -25,22 +31,35 @@ inv_media_type = {v: k for k, v in classify.media_type.items()}
 
 
 string_vars = []
+lables = []
+comboboxes = []
 for k,v in prediction.items():
-    string_vars.append(tk.StringVar())
-
-
-for k, v in prediction.items():
-    n = string_vars[i-1] 
+    n = tk.StringVar()
+    string_vars.append(n)
     label = ttk.Label(window, text=k)
+    label.grid(column = 2, row = i)
+    lables.append(label)
     filechosen = ttk.Combobox(window, width=30, textvariable=n)
     filechosen['values'] = tuple(classify.media_type.values())
-    label.grid(column = 2, row = i)
     filechosen.grid(column = 1, row = i)
     ind = inv_media_type[v]
-    print(v)
-    print(ind)
     filechosen.current(ind)
+    comboboxes.append(filechosen)
     i += 1
+
+
+elements = []
+i = 1
+for k, v in prediction.items():
+    n = string_vars[i-1] 
+    label = lables[i-1]
+    combobox = comboboxes[i-1]
+    i += 1
+    elements.append((label, n))
+
+
+button = ttk.Button(window, text="Move Files", command=move_files)
+button.grid(column=1, row=i+5)
 
 
 
